e01a93728ec06e1890c1cb6f622b118f7cc94e2b
MD-Roger <mathilde.declas@gmail.com>
2024-02-22 16:45:17 +0100

    first pump

---
+++ test.rb
@@ -0,0 +1,39 @@
+
+checkerArray =%w[de De passe Passe chien Chien]
+iterateur = 0
+compteur = 0
+array = [[
+    "Ceci est un lorem ipsmum blablalbla de",
+    "Ceci est un lorem ipsmum blablalbla truc",
+    "Ceci est un lorem ipsmum blablalbla homme",
+    "Ceci est un lorem ipsmum blablalbla chien",
+    "Ceci est un lorem ipsmum blablalbla passe"
+    ],
+    [
+    "Ceci est un lorem ipsmum blablalbla de",
+    "Ceci est un lorem ipsmum blablalbla truc",
+    "Ceci est un lorem ipsmum blablalbla",
+    "Ceci est un lorem ipsmum blablalbla",
+    "Ceci est un lorem ipsmum blablalbla passe"
+    ],
+    [
+    "Ceci est un lorem ipsmum blablalbla de",
+    "Ceci est un lorem ipsmum blablalbla truc",
+    "Ceci est un lorem ipsmum blablalbla femme",
+    "Ceci est un lorem ipsmum blablalbla chien",
+    "Ceci est un lorem ipsmum blablalbla passe",
+    ]
+]
+
+print array[iterateur][compteur]
+
+
+array.each do iterateur
+
+
+def checker
+    if  checkerArray
+      
+    end
+  
+end
